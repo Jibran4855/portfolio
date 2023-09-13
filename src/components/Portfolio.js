b0f35608be4e1862386774a7cf7b600664fc2da9
Jibran <jibrannaeem4855@gmail.com>
2023-09-13 17:58:30 +0500

    Generalize-portfolio-project

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,59 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "../assets/styles/Portfolio.css";
 import newmethods from "../assets/images/learnnewmethods.png";
+import PortfolioProject from "./PortfolioProject";
 
 const Portfolio = (props) => {
+  const [websiteDetails, setWebsiteDetail] = useState([
+    {
+      name: "Learning Website",
+      paragraph:
+        "This is my learning purpose website to developed in ReactJS FrontEnd Project in 2 days. Basically its a challege for me to developthis website in minimum days. And I Completed the Challenge in 2days by giving my 100% effort.",
+      technologies: ["ReactJS", "CSS"],
+      picture: newmethods,
+      githublink: "https://github.com/Jibran4855/LearningNewMethods",
+      livewebsitelink: "https://jibran4855.github.io/LearningNewMethods/",
+    },
+  ]);
+
   return (
     <div className="Portfolio-class">
       <div className="container">
         <div className="top">
           <h1>Portfolio</h1>
-          <h2>Each project is a unique piece of development </h2>
+          <h2>Each project is a unique piece of development.</h2>
         </div>
-        <div className="bottom">
-          <div className="containerb">
-            <div className="leftb">
-              <img src={newmethods} />
-            </div>
-            <div className="rightb">
-              <div className="topb">
-                <h3>Learning Website</h3>
-              </div>
-              <div className="midb">
-                <p>
-                  This is my learning purpose website to developed in ReactJS
-                  Front End Project in 2 days. Basically it's a challege for me
-                  to develop this website in minimum days. And I Completed the
-                  Challenge in 2 days by giving my 100% effort.
-                </p>
-                <div className="midbtn">
-                  <span>ReactJS</span>
-                  <span>CSS</span>
-                </div>
-              </div>
-              <div className="bottomb">
-                <a
-                  style={{ textDecoration: "none" }}
-                  href="https://jibran4855.github.io/LearningNewMethods/"
-                  target="_blank"
-                >
-                  <div className="livebtn">
-                    Live Demo <i class="fa fa-play-circle"></i>
-                  </div>
-                </a>
-                <a
-                  style={{ textDecoration: "none" }}
-                  href="https://github.com/Jibran4855/LearningNewMethods"
-                  target="_blank"
-                >
-                  <div className="gitbtn">
-                    Code <i class="fa fa-github"></i>
-                  </div>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
+        <PortfolioProject websiteDetails={websiteDetails} />
       </div>
     </div>
   );
